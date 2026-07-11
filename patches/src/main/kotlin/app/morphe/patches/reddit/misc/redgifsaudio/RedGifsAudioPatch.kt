c9252f0f31ce11436c6d1ec147abf764b5bb6a49
ekazrahul100-web <ekazrahul100@gmail.com>
2026-07-11 19:23:00 +0530

    fix: update imports to use Morphe patcher

--- patches/src/main/kotlin/app/morphe/patches/reddit/misc/redgifsaudio/RedGifsAudioPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/misc/redgifsaudio/RedGifsAudioPatch.kt
@@ -1,11 +1,11 @@
 package app.morphe.patches.reddit.misc.redgifsaudio
 
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
-import app.revanced.patcher.fingerprint.MethodFingerprint
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
+import app.morphe.patcher.data.BytecodeContext
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
+import app.morphe.patcher.fingerprint.MethodFingerprint
+import app.morphe.patcher.patch.BytecodePatch
+import app.morphe.patcher.patch.annotation.CompatiblePackage
+import app.morphe.patcher.patch.annotation.Patch
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
